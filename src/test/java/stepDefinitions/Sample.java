4d8416a21754719efb69b6d736aa0a804f3291de
kirandarapu <naveendarapu108@gmail.com>
2023-12-20 17:32:55 +0530

    this is my task commit

---
+++ src/test/java/stepDefinitions/Sample.java
@@ -0,0 +1,18 @@
+package stepDefinitions;
+
+public class Sample {
+	
+	int a=10;int b=15;
+	
+	public void sum() {
+		System.out.println(a+b);
+	}
+
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
